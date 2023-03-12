0b16fbe97487ea3303f7864736bd8fb4b448044d
Gaurav Deshmukh <gauravsdeshmukh@outlook.com>
2023-03-12 19:38:06 -0400

    Added argument parsers to train_phasegnn.py and evaluate_phasegnn.py

--- evaluate_phasegnn.py
+++ evaluate_phasegnn.py
@@ -4,19 +4,28 @@ Created on Sun Mar 12 17:59:04 2023
 
 @author: Gaurav Deshmukh
 """
-
+import argparse
 import torch
 from phase_utils import evaluate_molecule, evaluate_molecule_and_compare
 
 if __name__ == "__main__":
+    # Create argument parser
+    parser = argparse.ArgumentParser(prog="Evaluate PhaseGNN", description="Evaluate a Phase GNN model for a specified crtical property and molecule name")
+    parser.add_argument("-c","--critical-property",action="store")
+    parser.add_argument("-n","--name",action="store",nargs="+")
+    args = parser.parse_args()
+    
+    # Critical property
+    critical_property = str(args.critical_property)
+    
     # Specify molecule name
-    molecule_name = "acetaldehyde"
+    molecule_name = str(" ".join(args.name))
     
     # Use GPU if present
     use_GPU = True if torch.cuda.is_available() else False
     
     # Make model configuration dictionary 
-    config={"critical_property": "P_c", "node_vec_len": 40, "max_atoms": 100, "node_fea_len": 40, "hidden_fea_len": 40, "n_conv":4, "n_hidden": 2, "n_outputs": 1, "train_size": 0.7, "val_size": 0.2, "batch_size": 64, "use_GPU": use_GPU, "optimizer": "adam","epochs": 1000, "name": "Pc_fea40_conv4_hidden_dropout10", "p_dropout": 0.1}
+    config={"critical_property": critical_property, "node_vec_len": 40, "max_atoms": 100, "node_fea_len": 40, "hidden_fea_len": 40, "n_conv":4, "n_hidden": 2, "n_outputs": 1, "train_size": 0.7, "val_size": 0.2, "batch_size": 64, "use_GPU": use_GPU, "optimizer": "adam","epochs": 1000, "name": "{0}-fea40-conv4-hidden-dropout10".format(critical_property), "p_dropout": 0.1}
     
     # Evaluate critical property and print output
     evaluate_molecule_and_compare(molecule_name, config)
--- train_phasegnn.py
+++ train_phasegnn.py
@@ -4,12 +4,28 @@ Created on Sun Mar 12 17:56:25 2023
 
 @author: Gaurav Deshmukh
 """
+import argparse
 import numpy as np
 import torch
 
 from phase_utils import model_training, save_results
 
 if __name__ == "__main__":
+    # Create argument parser
+    parser = argparse.ArgumentParser(prog="Train PhaseGNN", description="Train a Phase GNN model for a specified crtical property and number of epochs")
+    parser.add_argument("-c","--critical-property",action="store")
+    parser.add_argument("-e","--epochs",action="store")
+    args = parser.parse_args()
+    
+    if args.epochs is None:
+        epochs = 1000
+    else:
+        epochs = int(args.epochs)
+    if args.critical_property is None:
+        raise Exception("Enter critical property (either P_c or T_c)")
+    else:
+        critical_property = str(args.critical_property)
+    
     # Set seed
     np.random.seed(1)
     torch.manual_seed(1)
@@ -18,7 +34,7 @@ if __name__ == "__main__":
     use_GPU = True if torch.cuda.is_available() else False
     
     # Make model configuration dictionary 
-    config={"critical_property": "P_c", "node_vec_len": 40, "max_atoms": 100, "node_fea_len": 40, "hidden_fea_len": 40, "n_conv":4, "n_hidden": 2, "n_outputs": 1, "train_size": 0.7, "val_size": 0.2, "batch_size": 64, "use_GPU": use_GPU, "optimizer": "adam","epochs": 1000, "name": "Pc_fea40_conv4_hidden_dropout10", "p_dropout": 0.1}
+    config={"critical_property": critical_property, "node_vec_len": 40, "max_atoms": 100, "node_fea_len": 40, "hidden_fea_len": 40, "n_conv":4, "n_hidden": 2, "n_outputs": 1, "train_size": 0.7, "val_size": 0.2, "batch_size": 64, "use_GPU": use_GPU, "optimizer": "adam","epochs": epochs, "name": "{0}-fea40-conv4-hidden-dropout10".format(critical_property), "p_dropout": 0.1}
     
     # Perform model training
     train_losses,val_losses, train_errors, val_errors, output_true, output_pred, output_smiles=model_training(config)
